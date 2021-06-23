986f8be4ef030e468b53bc9c6aad58c3ce7f6d53
MHalseyPerry <milo.halseyperry@gmail.com>
2021-06-23 15:24:46 +0100

    whole table row is now clickable.

--- resources/js/components/Tool.vue
+++ resources/js/components/Tool.vue
@@ -15,7 +15,7 @@
           <tr>
             <th class="w-3/4" colspan="1" scope="col">Files</th>
             <th class="w-1/4" colspan="1">
-              {{ files.length ? 'Download' : '' }}
+              {{ files.length ? "Download" : "" }}
             </th>
           </tr>
         </thead>
@@ -45,25 +45,28 @@
             </td>
           </tr>
           <tr v-for="directory in directories" :key="directory">
-            <td colspan="2">
-              <div class="flex flex-row items-center">
-                <svg
-                  class="mr-2"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  width="24"
-                  height="24"
-                >
-                  <path
-                    class="heroicon-ui"
-                    d="M20 6a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V6c0-1.1.9-2 2-2h7.41l2 2H20zM4 6v12h16V8h-7.41l-2-2H4z"
-                  />
-                </svg>
-                <button @click="openDirectory(directory)">
-                  {{ directory }}
-                </button>
-              </div>
-            </td>
+            <button @click="openDirectory(directory)">
+              <td colspan="2">
+                <div class="flex flex-row items-center">
+                  <svg
+                    class="mr-2"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    width="24"
+                    height="24"
+                  >
+                    <path
+                      class="heroicon-ui"
+                      d="M20 6a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V6c0-1.1.9-2 2-2h7.41l2 2H20zM4 6v12h16V8h-7.41l-2-2H4z"
+                    />
+                  </svg>
+
+                  <p>
+                    {{ directory }}
+                  </p>
+                </div>
+              </td>
+            </button>
           </tr>
           <tr v-for="file in files" :key="file">
             <td>{{ file }}</td>
@@ -100,13 +103,13 @@
 export default {
   metaInfo() {
     return {
-      title: 'FileExplorer',
+      title: "FileExplorer",
     };
   },
 
   data() {
     return {
-      title: '',
+      title: "",
       disks: [],
       fetching: false,
       selectedDisk: null,
@@ -133,15 +136,15 @@ export default {
 
   methods: {
     getFiles() {
-      let path = this.path.join('/');
+      let path = this.path.join("/");
       console.log(path, this.selectedDisk);
       this.fetching = true;
       Nova.request()
         .get(
-          '/nova-vendor/file-explorer/files?disk=' +
+          "/nova-vendor/file-explorer/files?disk=" +
             this.selectedDisk +
-            '&path=' +
-            path,
+            "&path=" +
+            path
         )
         .then((response) => {
           this.files = response.data.files;
@@ -152,7 +155,7 @@ export default {
 
     getConfig() {
       Nova.request()
-        .get('/nova-vendor/file-explorer/files/disks')
+        .get("/nova-vendor/file-explorer/files/disks")
         .then((response) => {
           this.disks = response.data.disks;
           this.selectedDisk = response.data.default;
